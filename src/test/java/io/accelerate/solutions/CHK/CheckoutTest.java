ab41267531f1a71aade51c5d26b4e1e2bc6f7d75
sbx_user1051 <sbx_user1051@169.254.69.173>
2026-02-05 11:52:32 +0000

    Thu Feb 05 11:52:32 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/CheckoutTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutTest.java
@@ -1,6 +1,7 @@
 package io.accelerate.solutions.CHK;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CheckoutTest {
@@ -15,25 +16,26 @@ public class CheckoutTest {
         final String sku3 = "ABBC";
         final String sku4 = "CCD";
         final String sku5 = "DDAAB";
+        final String sku6 = "";
 
         assertEquals(115, (int) checkoutSolution.checkout(sku1));
         assertEquals(165, (int) checkoutSolution.checkout(sku2));
         assertEquals(115, (int) checkoutSolution.checkout(sku3));
         assertEquals(55, (int) checkoutSolution.checkout(sku4));
         assertEquals(160, (int) checkoutSolution.checkout(sku5));
+        assertEquals(0, (int) checkoutSolution.checkout(sku6));
     }
 
     @Test
     void skusOnlyIncludeAcceptedProductIdentifiers() {
         final String nonValidSku1 = "ADFCBA";
-        final String nonValidSku2 = "  ";
         final String nonValidSku3 = null;
         final String nonValidSku4 = "RT";
 
         assertEquals(-1, checkoutSolution.checkout(nonValidSku1));
-        assertEquals(-1, checkoutSolution.checkout(nonValidSku2));
         assertEquals(-1, checkoutSolution.checkout(nonValidSku3));
         assertEquals(-1, checkoutSolution.checkout(nonValidSku4));
     }
 }
+
 
