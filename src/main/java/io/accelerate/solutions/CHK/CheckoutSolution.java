ebfd18d3c46b00eca45bf43fc889126d54a70a6e
sbx_user1051 <sbx_user1051@169.254.69.173>
2026-02-05 12:22:39 +0000

    Thu Feb 05 12:22:39 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -3,21 +3,27 @@ package io.accelerate.solutions.CHK;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
 
-        if (skus != null && skus.chars().allMatch(ch -> ch == 'A' || ch == 'B' || ch == 'C' || ch == 'D')) {
+        if (skus != null && skus.chars()
+                .allMatch(ch -> ch == 'A' || ch == 'B' || ch == 'C' || ch == 'D' || ch == 'E')) {
             var acount = (int) skus.chars().filter(a -> a == 'A').count();
             var bcount = (int) skus.chars().filter(b -> b == 'B').count();
             var ccount = (int) skus.chars().filter(c -> c == 'C').count();
             var dcount = (int) skus.chars().filter(d -> d == 'D').count();
+            var ecount = (int) skus.chars().filter(d -> d == 'E').count();
 
             var cprice = ccount * 20;
             var dprice = dcount * 15;
+            var eprice = ecount * 40;
             var aprice = ((acount / 3) * 130) + ((acount % 3) * 50);
-            var bprice = ((bcount / 2) * 45) + ((bcount % 2) * 30);
+            if (ecount >= 2) {
 
-            return cprice + dprice + aprice + bprice;
+                var bprice = ((bcount / 2) * 45) + ((bcount % 2) * 30);
+            }
+            return cprice + dprice + aprice + bprice + eprice;
         }
         return -1;
     }
 }
+
 
 
