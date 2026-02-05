2b6968c3018326a2caf406b0744fa4c16da3a914
sbx_user1051 <sbx_user1051@169.254.69.173>
2026-02-05 11:29:54 +0000

    Thu Feb 05 11:29:54 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -3,20 +3,21 @@ package io.accelerate.solutions.CHK;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
 
-       // if(!skus.chars().allMatch()) {return -1;}
+        if(skus != null && skus.chars().allMatch(ch -> ch=='A' || ch=='B' || ch=='C' || ch=='D')) {
+            var acount = (int) skus.chars().filter(a -> a == 'A').count();
+            var bcount = (int) skus.chars().filter(b -> b == 'B').count();
+            var ccount = (int) skus.chars().filter(c -> c == 'C').count();
+            var dcount = (int) skus.chars().filter(d -> d == 'D').count();
 
-        var acount = (int) skus.chars().filter(a -> a == 'A').count();
-        var bcount = (int) skus.chars().filter(b -> b == 'B').count();
-        var ccount = (int) skus.chars().filter(c -> c == 'C').count();
-        var dcount = (int) skus.chars().filter(d -> d == 'D').count();
+            var cprice = ccount * 20;
+            var dprice = dcount * 15;
+            var aprice = ((acount / 3) * 130) + ((acount % 3) * 50);
+            var bprice = ((bcount / 2) * 45) + ((bcount % 2) * 30);
 
-        var cprice = ccount * 20;
-        var dprice = dcount * 15;
-        var aprice = ((acount / 3) * 130) + ((acount % 3) * 50);
-        var bprice = ((bcount / 2) * 45) + ((bcount % 2) * 30);
-
-        return cprice + dprice + aprice + bprice;
-
+            return cprice + dprice + aprice + bprice;
+        }
+        return -1;
     }
 }
+
 
--- src/test/java/io/accelerate/solutions/CHK.java
+++ src/test/java/io/accelerate/solutions/CHK.java
@@ -1,2 +1,17 @@
-package io.accelerate.solutions;public class CHK {
+package io.accelerate.solutions;
+
+import org.junit.jupiter.api.Test;
+
+public class CHK {
+
+    @Test
+    void calculateTotalProductsPrice() {
+
+        final String option1 = "ABCD";
+        final String option2 = "AAABCBB";
+        final String option3 = "ABBACD";
+        final String option4 = "ABCCDD";
+    }
+
 }
+
