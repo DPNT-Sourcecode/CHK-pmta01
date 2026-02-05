b9dec29656caf1985a1aeb0d940ce5958ca529bd
sbx_user1051 <sbx_user1051@169.254.69.173>
2026-02-05 11:20:54 +0000

    Thu Feb 05 11:20:54 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -3,6 +3,20 @@ package io.accelerate.solutions.CHK;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
 
-        skus.chars().allMatch()
+       // if(!skus.chars().allMatch()) {return -1;}
+
+        var acount = (int) skus.chars().filter(a -> a == 'A').count();
+        var bcount = (int) skus.chars().filter(b -> b == 'B').count();
+        var ccount = (int) skus.chars().filter(c -> c == 'C').count();
+        var dcount = (int) skus.chars().filter(d -> d == 'D').count();
+
+        var cprice = ccount * 20;
+        var dprice = dcount * 15;
+        var aprice = ((acount / 3) * 130) + ((acount % 3) * 50);
+        var bprice = ((bcount / 2) * 45) + ((bcount % 2) * 30);
+
+        return cprice + dprice + aprice + bprice;
+
     }
 }
+
---
+++ src/test/java/io/accelerate/solutions/CHK.java
@@ -0,0 +1,2 @@
+package io.accelerate.solutions;public class CHK {
+}
